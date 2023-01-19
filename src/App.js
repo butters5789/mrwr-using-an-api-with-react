55bc8e4e2bdcb98983a0b00ac2f480527a222a66
butters5789 <butters5789@gmail.com>
2023-01-18 21:20:29 -0700

    link files and pass props and callbacks

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SearchBar from './components/SearchBar';
+
+function App() {
+  const handleSubmit = (term) => {
+    console.log('search', term);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
+}
+
+export default App;
